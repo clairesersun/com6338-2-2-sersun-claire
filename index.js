2424cf77671c59cddb6d14e1057f1213b73e991f
Claire Sersun <claire.sersun@gmail.com>
2022-05-13 22:59:35 -0400

    script added

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+const currentYear = new Date().getFullYear()
+
+function greet() {
+    let name = prompt('What is your name?')
+    alert('Hello, ' + name)
+    let givenAge = prompt('How old are you?')
+    let age = parseInt(givenAge)
+    let thisYearBorn = confirm('Have you had a birthday yet this year?')
+    let yearBorn = () => {
+        if (thisYearBorn) {
+            return currentYear - age
+        } else {
+            return currentYear - age - 1
+        }
+    }
+    alert('You were born in ' + yearBorn())
+}
